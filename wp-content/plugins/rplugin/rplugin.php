3e9de3b822d75b076bfd4d5a07e61abe8e28f17f
Rasmus <rasmus.laane@khk.ee>
2019-01-16 14:48:53 +0200

    modified plugin `RPlugin` version 1.0 (after activation of `RPlugin` version 1.0)

--- wp-content/plugins/rplugin/rplugin.php
+++ wp-content/plugins/rplugin/rplugin.php
@@ -10,15 +10,25 @@ Author URI: rasmuslaane.ikt.khk.ee/wordpress
 */
 
 
-// include('includes/function.php');
 
-function rl_alert_box(){
+add_action( 'wp_footer', 'my_function' );
 
-echo "<script type='text/javascript'>alert(\"$error\");</script>";
-
+function my_function() {
+  echo 'hello world';
 }
 
 
-add_action( 'init', 'rl_alert_box' );
+add_action('admin_menu', 'my_admin_menu');
+
+function my_admin_menu () {
+  add_management_page('Footer Text', 'Footer Text', 'manage_options', __FILE__, 'footer_text_admin_page');
+}
+
+function footer_text_admin_page () {
+  echo 'this is where we will edit the variable';
+}
+
+
+ add_management_page( $page_title, $menu_title, $capability, $menu_slug, $function );
 
 ?>
