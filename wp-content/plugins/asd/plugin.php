5ef6aa02a94be32a27964a1ae1b12f996a769198
Rasmus <rasmus.laane@khk.ee>
2019-01-16 15:15:22 +0200

    modified plugin `asd` version 1.0 (after activation of `asd` version 1.0)

---
+++ wp-content/plugins/asd/plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Plugin name: asd
+Description: Sample Plugin
+Version: 1.0
+Author: Rasmus Laane
+Author URI: rasmuslaane.ikt.khk.ee/wordpress
+
+*/
+
+
+add_action( 'wp_footer', 'my_function' );
+
+function my_function() {
+  echo 'hello world';
+}
+
+
+?>
