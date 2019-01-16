f62abb4d6d84969040b0c135c78eeed6fe2cfe2c
Rasmus <rasmus.laane@khk.ee>
2019-01-16 16:27:13 +0200

    modified plugin `Test` version 1.0 (after deactivation of `Test` version 1.0)

--- wp-content/plugins/test/test.php
+++ wp-content/plugins/test/test.php
@@ -1,22 +1,8 @@
 <?php
-
 /**
-* Plugin Name: Test plugin
-* Plugin URI: http://www.mainwp.com
-* Description: This plugin does some stuff with WordPress
-* Version: 1.0.0
-* Author: Your Name Here
-* Author URI: http://www.mainwp.com
-* License: GPL2
-*/
-
-?>
-
-
-add_action('wp_loaded', 'my_func');
-
-function my_func() {
-
-	echo 'hello world';
-
-}
+* Plugin Name: Test
+* Description: This is the very first plugin I ever created.
+* Version: 1.0
+* Author: Rasmus Laane
+* Author URI: http://rasmuslaane.ikt.khk.ee
+**/
